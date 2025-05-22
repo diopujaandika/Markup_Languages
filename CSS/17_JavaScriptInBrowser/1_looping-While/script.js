22c0a41fbcd6383f0385e8083c95ee38cb5cc020
diopujaandika <dpujaandika17@gmail.com>
2025-05-22 15:09:30 +0700

    push again

---
+++ CSS/17_JavaScriptInBrowser/1_looping-While/script.js
@@ -0,0 +1,23 @@
+var ulang = true
+while(ulang){
+    console.log('hellow world')
+    ulang=confirm('lagi?')
+}
+
+var nilaiAwal = 1
+while(nilaiAwal <= 5){
+    console.log("Hello World")
+    nilaiAwal++
+}
+
+var nilaiAkhir = 1
+while(nilaiAkhir <= 10){
+    console.log('Nilai Akhir ', nilaiAkhir)
+    nilaiAkhir++
+}
+
+var angkot = 1
+while(angkot <= 10){
+    console.log('Angkot nomor', angkot, 'beroprasi dengan baik')
+    angkot++
+}
