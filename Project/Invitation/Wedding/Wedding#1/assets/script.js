854cda69b1be7c7fadaa3fe13f53e2873b2c2b09
Dio Puja Andika <dpujaandika17@gmail.com>
2026-09-15 12:31:41 +0700

    update | project

---
+++ Project/Invitation/Wedding/Wedding#1/assets/script.js
@@ -0,0 +1,37 @@
+const weddingDate = new Date("September 7, 2027 10:00:00").getTime();
+const countdown = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+    const days = Math.floor(
+        distance / (1000 * 60 * 60 * 24)
+    );
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) /
+        (1000 * 60)
+    );
+    const seconds = Math.floor(
+        (distance % (1000 * 60)) /
+        1000
+    );
+
+    document.getElementById("days").textContent =
+        String(days).padStart(2, "0");
+    document.getElementById("hours").textContent =
+        String(hours).padStart(2, "0");
+    document.getElementById("minutes").textContent =
+        String(minutes).padStart(2, "0");
+    document.getElementById("seconds").textContent =
+        String(seconds).padStart(2, "0");
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("days").textContent = "00";
+        document.getElementById("hours").textContent = "00";
+        document.getElementById("minutes").textContent = "00";
+        document.getElementById("seconds").textContent = "00";
+    }
+}, 1000);
